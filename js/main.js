ad2a76f413921d9fddce7bebf11a3278625d1369
SeregaLSA <vip.sergeylsa@mail.ru>
2023-03-10 20:33:42 +0600

    1212

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+});
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+  new Swiper('.swiper', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+    });
+  
+  });
